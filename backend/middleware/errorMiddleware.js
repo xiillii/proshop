a75c44af4af42e8c5c64b7ba99d9bcfc10ea73c1
José Alonso <josealonso@romero-matias.com>
2022-03-12 23:01:38 -0600

    custom error handling

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,17 @@
+const notFound = (req, res, next) => {
+  const error = new Error(`Not found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    timestamp: new Date(Date.now()),
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+};
+
+export { errorHandler, notFound };
