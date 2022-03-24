a892c81daa339abd22b6846d394158af076c7223
Jose Alonso <mail@josealonso.dev>
2022-03-23 20:47:13 -0600

    User registration and password encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,11 +4,13 @@ import {
   authUser,
   getUserProfile,
   getUsers,
+  registerUser,
 } from '../controllers/userController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getUsers);
 router.post('/login', authUser);
 router.route('/profile').get(protect, getUserProfile);
+router.route('/').post(registerUser);
 
 export default router;
